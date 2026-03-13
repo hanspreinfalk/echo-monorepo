e471329d5ee73f1c61aa795466526d486f5c96c2
RoboticsAAA20Group <hans.preinfalk.davila@gmail.com>
2026-03-13 00:34:12 -0600

    08: theme

--- apps/web/modules/dashboard/ui/components/dashboard-sidebar.tsx
+++ apps/web/modules/dashboard/ui/components/dashboard-sidebar.tsx
@@ -114,6 +114,9 @@ export const DashboardSidebar = () => {
                                         asChild 
                                         tooltip={item.title}
                                         isActive={isActive(item.url)}
+                                        className={cn(
+                                            isActive(item.url) && 'bg-linear-to-b from-sidebar-primary to-[#0b63f3]! text-sidebar-primary-foreground! hover:to-[#0b63f3]/90!'
+                                        )}
                                     >
                                         <Link href={item.url}>
                                             <item.icon className="size-4" />
@@ -137,6 +140,9 @@ export const DashboardSidebar = () => {
                                         asChild
                                         tooltip={item.title}
                                         isActive={isActive(item.url)}
+                                        className={cn(
+                                            isActive(item.url) && 'bg-linear-to-b from-sidebar-primary to-[#0b63f3]! text-sidebar-primary-foreground! hover:to-[#0b63f3]/90!'
+                                        )}
                                     >
                                         <Link href={item.url}>
                                             <item.icon className="size-4" />
@@ -160,6 +166,9 @@ export const DashboardSidebar = () => {
                                         asChild
                                         tooltip={item.title}
                                         isActive={isActive(item.url)}
+                                        className={cn(
+                                            isActive(item.url) && 'bg-linear-to-b from-sidebar-primary to-[#0b63f3]! text-sidebar-primary-foreground! hover:to-[#0b63f3]/90!'
+                                        )}
                                     >
                                         <Link href={item.url}>
                                             <item.icon className="size-4" />
