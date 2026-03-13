1e27f7452b3613503037199b8be25777ee6835e9
RoboticsAAA20Group <hans.preinfalk.davila@gmail.com>
2026-03-12 22:11:14 -0600

    07: dashboard layout

---
+++ apps/web/modules/dashboard/ui/layouts/dashboard-layout.tsx
@@ -0,0 +1,26 @@
+import { AuthGuard } from "@/modules/auth/ui/components/auth-guard";
+import { OrganizationGuard } from "@/modules/auth/ui/components/organization-guard";
+import { SidebarProvider } from "@workspace/ui/components/sidebar";
+import { TooltipProvider } from "@workspace/ui/components/tooltip";
+import { cookies } from "next/headers";
+import { DashboardSidebar } from "@/modules/dashboard/ui/components/dashboard-sidebar";
+
+export async function DashboardLayout({ children }: { children: React.ReactNode }) {
+    const cookieStore = await cookies()
+    const defaultOpen = cookieStore.get('sidebar_state')?.value === 'true'
+
+    return (
+        <AuthGuard>
+            <OrganizationGuard>
+                <TooltipProvider>
+                    <SidebarProvider defaultOpen={defaultOpen}>
+                        <DashboardSidebar />
+                        <main className="flex flex-1 flex-col">
+                            {children}
+                        </main>
+                    </SidebarProvider>
+                </TooltipProvider>
+            </OrganizationGuard>
+        </AuthGuard>
+    )
+}
