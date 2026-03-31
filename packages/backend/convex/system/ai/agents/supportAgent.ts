b4de5a781a5638e26b8e101c90a162519f76a4dd
RoboticsAAA20Group <hans.preinfalk.davila@gmail.com>
2026-03-31 11:53:21 -0700

    14: ai agents

---
+++ packages/backend/convex/system/ai/agents/supportAgent.ts
@@ -0,0 +1,8 @@
+import { components } from "../../../_generated/api";
+import { Agent } from "@convex-dev/agent";
+import { openai } from "@ai-sdk/openai";
+
+export const supportAgent = new Agent(components.agent, {
+    chat: openai.chat("gpt-4o-mini"),
+    instructions: "You are a customer support agent"
+});
